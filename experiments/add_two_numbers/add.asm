aaea4ff243c333a1829d13c1bcfb187dd32aed18
Enzo Ribas <enzorochaleitedinizribas@gmail.com>
2025-08-11 15:29:49 -0300

    changing add_two_numbers experiment to work on linux

--- experiments/add_two_numbers/add.asm
+++ experiments/add_two_numbers/add.asm
@@ -3,6 +3,6 @@ global add_numbers
 section .text
 
 add_numbers:
-    mov rax, rcx      ; move first argument to rax
-    add rax, rdx      ; add second argument to rax
+    mov rax, rdi      ; move first argument to rax
+    add rax, rsi      ; add second argument to rax
     ret               ; return result in rax
